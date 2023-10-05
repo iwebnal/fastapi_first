b75ff83e69dd0c7213a102cceefde09a6a9653a4
iwebnal <iwebnal@gmail.com>
2023-10-06 00:03:40 +0300

    Validate client and server data

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
-from fastapi import FastAPI
+from datetime import datetime
+from enum import Enum
+from typing import List, Optional
 
-app = FastAPI()
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel, Field, ValidationError
+
+app = FastAPI(
+    title="My FastApi app"
+)
+
+
+@app.exception_handler(ValidationError)
+async def validation_exeption_handler(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code = status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content = jsonable_encoder({"detail": exc.errors()})
+    )
 
 
 fake_users = [
-    {"id": 1, "role": "admin", "name": "John"},
-    {"id": 2, "role": "user", "name": "Mike"},
-    {"id": 3, "role": "manager", "name": "Ann"},
-    {"id": 4, "role": "emploee", "name": "Ivan"}
+    {"id": 1, "role": "admin", "name": ["John"], "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "newbie"}
+    ]},
+    {"id": 2, "role": "user", "name": "Mike", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
+    {"id": 3, "role": "manager", "name": "Ann", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "newbie"}
+    ]},
+    {"id": 4, "role": "emploee", "name": "Ivan", "degree": [
+        {"id": 1, "created_at": "2020-01-01T00:00:00", "type_degree": "expert"}
+    ]},
+    {"id": 5, "role": "emploee1", "name": "Ivan"}
 ]
 
 fake_users1 = [
@@ -28,7 +54,26 @@ def say_hello():
     return "Hello !!!"
 
 
-@app.get("/users/{user_id}")
+class DegreeType(Enum):
+    newbie = "newbie"
+    expert = "expert"
+
+
+class Degree(BaseModel):
+    id: int
+    created_at: datetime
+    type_degree: DegreeType
+
+
+class User(BaseModel):
+    id: int
+    role: str
+    name: str
+    degree: Optional[List[Degree]] = []   # optional property
+
+
+# validation of data from the server
+@app.get("/users/{user_id}", response_model=List[User])
 def get_user(user_id: int):
     return [user for user in fake_users if user.get("id") == user_id]
 
@@ -36,6 +81,23 @@ def get_user(user_id: int):
 @app.get("/trades")
 def get_trades(limit: int = 1, offset: int = 0):
     return fake_trades[offset:][:limit]
+
+
+#  validation with pydantic
+class Trade(BaseModel):
+    id: int
+    user_id: int
+    currency: str = Field(max_length=10)
+    side: str
+    price: float = Field(ge=0)
+    amount: float
+
+
+# client data validation
+@app.post("/trades")
+def add_trades(trades: List[Trade]):
+    fake_trades.extend(trades)
+    return {"status": 200, "data": fake_trades}
 
 
 @app.post("/users/{user_id}")
